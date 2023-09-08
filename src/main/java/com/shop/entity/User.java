2c205b77afc299dc519d36042eb522a254c6f23d
xuanhung2002 <xuanhung09052002@gmail.com>
2023-09-08 12:16:22 +0700

    commit to test deploy

--- src/main/java/com/shop/entity/User.java
+++ src/main/java/com/shop/entity/User.java
@@ -11,7 +11,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 @Entity
-@Table(name = "user")
+@Table(name = "appUser")
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/main/java/com/shop/initalization/RoleInitializer.java
@@ -0,0 +1,30 @@
+package com.shop.initalization;
+
+import com.shop.entity.Role;
+import com.shop.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoleInitializer implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+
+    public RoleInitializer(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        createRoleIfNotFound("ADMIN");
+        createRoleIfNotFound("USER");
+    }
+
+    private void createRoleIfNotFound(String roleName) {
+        if (roleRepository.findByName(roleName) == null) {
+            Role role = new Role();
+            role.setName(roleName);
+            roleRepository.save(role);
+        }
+    }
+}
